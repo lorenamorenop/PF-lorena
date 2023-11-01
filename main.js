e45d0facc33250ee1406baf5b548d2c636437799
Lorena Moreno <lorenamorenopertusi@gmail.com>
2023-11-01 17:00:47 -0300

    agrego una funcion

--- main.js
+++ main.js
@@ -15,19 +15,3 @@ function calcularsueldo(sueldobruto, obrasocial = 0.03, jubilacion = 0.11, ley19
     return sueldobruto - descuentos;
 }
 
-let ingresodatos = true;
-while (ingresodatos) {
-    let ingresosueldo = parseFloat(prompt("Por favor, introduzca su ingreso mensual en peso, sin simbolos"));
-    let obrasocial = parseFloat(prompt("Ingrese la tasa de Obra social (por ejemplo, 0.03 para 3%) sin simbolos:"));
-    let jubilacion = parseFloat(prompt("Ingrese la tasa de Jubilación (por ejemplo, 0.11 para 11%) sin simbolos:"));
-    let ley19032 = parseFloat(prompt("Ingrese la tasa de la Ley 19032 (por ejemplo, 0.03 para 3%) sin simbolos:"));
-    
-    let sueldoneto = calcularsueldo(ingresosueldo, obrasocial, jubilacion, ley19032);
-    alert("Su sueldo neto a cobrar es de: $" + sueldoneto);
-    
-    let accionconfirmar = prompt("¿Desea calcular otro sueldo?  Ingrese SI o N0");
-    if (accionconfirmar == "NO") {
-        ingresodatos = false;
-    }
-}
-alert("Gracias, hasta pronto.");
