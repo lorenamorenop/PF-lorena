eeddf9865dd8c26973e5a05cd98f095feea97b1c
Lorena Moreno <lorenamorenopertusi@gmail.com>
2023-11-13 22:49:56 -0300

    Test prueba

--- main.js
+++ main.js
@@ -1,33 +1,73 @@
-let usuario = true;
-while (usuario) {
-    let nombre = prompt("Ingresa tu nombre");
-    if (nombre != "") {
-        alert("Bienvenid@ " + nombre + ".");
-        usuario = false;
-    } else {
-        alert("El usuario ingresado es incorrecto");
-    }
+
+
+//funcion constructora que luego usare en el array
+function Preguntas(enunciado, valor) {
+    this.enunciado = enunciado;
+    this.valor = valor;
 }
 
-function calcularsueldo(sueldobruto, obrasocial = 0.03, jubilacion = 0.11, ley19032 = 0.03) {
-    let descuentos = sueldobruto * (obrasocial + jubilacion + ley19032);
-    alert("El descuento total es: $" + descuentos);
-    return sueldobruto - descuentos;
+function comprobarRespuesta(preguntas, respuestaUsuario) {
+    return preguntas.valor === respuestaUsuario;
 }
 
-let ingresodatos = true;
-while (ingresodatos) {
-    let ingresosueldo = parseFloat(prompt("Por favor, introduzca su ingreso mensual en peso:"));
-    let obrasocial = parseFloat(prompt("Ingrese la tasa de Obra social (por ejemplo, 0.03 para 3%):"));
-    let jubilacion = parseFloat(prompt("Ingrese la tasa de Jubilación (por ejemplo, 0.11 para 11%):"));
-    let ley19032 = parseFloat(prompt("Ingrese la tasa de la Ley 19032 (por ejemplo, 0.03 para 3%):"));
-    
-    let sueldoneto = calcularsueldo(ingresosueldo, obrasocial, jubilacion, ley19032);
-    alert("Su sueldo neto a cobrar es de: $" + sueldoneto);
-    
-    let accionconfirmar = prompt("¿Desea calcular otro sueldo?  Ingrese SI o N0");
-    if (accionconfirmar == "NO") {
-        ingresodatos = false;
-    }
+//AGREGAR OBJETOS 
+let arrayDePreguntas = [
+    new Preguntas("La clorofila es responsable del color verde en las plantas",true),
+    new Preguntas("La energía no se crea ni se destruye, solo se transforma",true),
+    new Preguntas("El hombre pisó la luna por primera vez en el año 1950",false),
+    new Preguntas("Quién pintó 'El grito' fue Edvard Munch",true),
+    new Preguntas("España ganó su primer Mundial de fútbol en el año 2006.",false),
+    new Preguntas("El primer presidente de los Estados Unidos fue Thomas Jefferson", false),
+    new Preguntas("El hígado está en el lado derecho del cuerpo", true),
+    new Preguntas("Blancanieves fue la primera película de Disney", true),
+    new Preguntas("En un equipo de voleibol hay 7 jugadores", false),
+    new Preguntas("Leo Messi nacio en Salta, Argentina", false),
+    new Preguntas("El carbono está presente en absolutamente todas las moléculas orgánicas", true),
+    new Preguntas ("El Sol está más cerca de la Tierra en el mes de Diciembre.", true)
+]
+
+
+
+function ejecutarTest() {
+    let puntaje = 0;
+
+    arrayDePreguntas.forEach(function(preguntas) {
+        let respuesta;
+        do {
+            respuesta = prompt(preguntas.enunciado + " (Escribé verdadero o falso)").toLowerCase();
+            if (!comprobarRespuesta(preguntas, respuesta)) {
+                alert("Respuesta incorrecta. Por favor, responde verdadero o falso.");
+            }
+        } while (!comprobarRespuesta(preguntas, respuesta));
+
+        puntaje++;
+    });
+
+    alert("Has obtenido " + puntaje + " respuestas correctas de " + preguntas.length);
 }
-alert("Gracias, hasta pronto.");
+
+
+ejecutarTest();
+
+//HACER UN SWITCH DE VALORES CASE SI RETURN 1 CASE NO RETURN -1 CASE INDIFERENTE RETURN 0
+// let valor = function (a, b, c) {
+//         a == 1;
+//         b == -1;
+//         c == 0;
+// }
+//function("VERDADERO", "FALSO")
+
+//let suma = suma + valor
+
+// for (let i = 0; i < arrayDePreguntas.length; i++) {
+//     let respuesta = ''
+//     while(respuesta !== 'verdadero' || respuesta !== 'falso'){
+//         respuesta = prompt (arrayDePreguntas[i]).toLowerCase
+//         if ( respuesta !== 'verdadero' || respuesta !== 'falso') {
+//             alert("solo se acepta VERDADERO ó FALSO")
+//         }
+        
+//     }
+
+// }
+
