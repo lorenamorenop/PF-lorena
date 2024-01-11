8a1e9d78852295e02d31380ee71bb189709aeb64
Lorena Moreno <lorenamorenopertusi@gmail.com>
2024-01-11 17:56:42 -0300

    agrego fetch

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function mostrarMensaje(idDiv, mensaje) {
     div.innerHTML = "<p>" + mensaje + "</p>";
 }
 
+
+
 //creo la funcion saludo (oculto el boton ingresar) y muestro mi contenedor
 function saludo() {
     let respuestaSaludo = nombreUsuario.value;
@@ -36,22 +38,15 @@ function Preguntas(enunciado, valor) {
 function comprobarRespuesta(preguntas, respuestaUsuario) {
     return preguntas.valor === respuestaUsuario;
 }
-
-//AGREGAR OBJETOS 
-    let arrayDePreguntas = [
-    new Preguntas("La clorofila es responsable del color verde en las plantas",true),
-    new Preguntas("La energía no se crea ni se destruye, solo se transforma",true),
-    new Preguntas("El hombre pisó la luna por primera vez en el año 1950",false),
-    new Preguntas("Quién pintó 'El grito' fue Edvard Munch",true),
-    new Preguntas("España ganó su primer Mundial de fútbol en el año 2006.",false),
-    new Preguntas("El primer presidente de los Estados Unidos fue Thomas Jefferson", false),
-    new Preguntas("El hígado está en el lado derecho del cuerpo", true),
-    new Preguntas("Blancanieves fue la primera película de Disney", true),
-    new Preguntas("En un equipo de voleibol hay 7 jugadores", false),
-    new Preguntas("Leo Messi nacio en Salta, Argentina", false),
-    new Preguntas("El carbono está presente en absolutamente todas las moléculas orgánicas", true),
-    new Preguntas ("El Sol está más cerca de la Tierra en el mes de Diciembre.", true)
-]
+function obtenerPreguntas() {
+    fetch('preguntas.json')
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            arrayDePreguntas = data;
+            mostrarPregunta(arrayDePreguntas[currentIndex]);
+        })
+        .catch(error => console.error('Error al obtener las preguntas:', error));
+}
 
 let puntaje = 0;
 let currentIndex = 0;
@@ -147,4 +142,4 @@ function mostrarResultado() {
     contenedor.innerHTML = "<p>Has obtenido " + puntaje + " respuestas correctas de " + arrayDePreguntas.length + "</p>";
 }
 
-mostrarPregunta(arrayDePreguntas[currentIndex]);
+obtenerPreguntas();
